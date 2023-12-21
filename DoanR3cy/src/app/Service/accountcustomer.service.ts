824d678919b92d3d7af4a1c287ed6710473b79ab
lttuyetanh <lttuyetanh273@gmail.com>
2023-12-22 02:08:22 +0700

    Update lại API

--- DoanR3cy/src/app/Service/accountcustomer.service.ts
+++ DoanR3cy/src/app/Service/accountcustomer.service.ts
@@ -1,8 +1,4 @@
-import {
-  HttpClient,
-  HttpErrorResponse,
-  HttpHeaders,
-} from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http'
 import { Injectable } from '@angular/core';
 import { catchError, map, Observable, retry, throwError } from 'rxjs';
 import { AccountCustomer } from '../Interface/AccountCustomer';
@@ -11,42 +7,44 @@ import { AccountCustomer } from '../Interface/AccountCustomer';
   providedIn: 'root'
 })
 export class AccountcustomerService {
+  private apiUrl = 'http://localhost:3000';
 
   constructor(private _http: HttpClient) { }
 
+  checkPhoneNumberExist(phonenumber: string): Observable<any> {
+    const headers = new HttpHeaders().set(
+      'Content-Type',
+      'text/plain;charset=utf8'
+    );
+    const requestOptions: Object = {
+      headers: headers,
+      responseType: 'text',
+    };
+    return this._http.get<any>(`${this.apiUrl}/accounts/${phonenumber}`, requestOptions).pipe(
+      map(res => JSON.parse(res) as Array<AccountCustomer>),
+      retry(3),
+      catchError(this.handleError)
+    );
+  }
 
-        checkPhoneNumberExist(phonenumber: string): Observable<any> {
-        const headers = new HttpHeaders().set(
-          'Content-Type',
-          'text/plain;charset=utf8'
-        )
-        const requestOptions: Object = {
-          headers: headers,
-          responseType: 'text',
-        }
-        return this._http.get<any>("/accounts/" + phonenumber, requestOptions).pipe(
-          map(res => JSON.parse(res) as Array<AccountCustomer>),
-          retry(3),
-          catchError(this.handleError)
-        );
-      }
-      handleError(error: HttpErrorResponse) {
-        return throwError(() => new Error(error.message));
-      }
-      //Post 1 account vào database
-      postAccount(aAccount:any):Observable<any> {
-        const headers = new HttpHeaders().set(
-          'Content-Type',
-          'application/json;charset=utf-8'
-        )
-        const requestOptions: Object = {
-          headers: headers,
-          responseType: 'json',
-        }
-        return this._http.post<any>("/accounts", JSON.stringify(aAccount), requestOptions).pipe(
-          map(res=> JSON.parse(res) as AccountCustomer),
-          retry(3),
-          catchError(this.handleError)
-        )
-      }
+  handleError(error: HttpErrorResponse) {
+    return throwError(() => new Error(error.message));
+  }
+
+  // Post 1 account vào database
+  postAccount(aAccount: any): Observable<any> {
+    const headers = new HttpHeaders().set(
+      'Content-Type',
+      'application/json;charset=utf-8'
+    );
+    const requestOptions: Object = {
+      headers: headers,
+      responseType: 'json',
+    };
+    return this._http.post<any>(`${this.apiUrl}/accounts`, JSON.stringify(aAccount), requestOptions).pipe(
+      map(res => JSON.parse(res) as AccountCustomer),
+      retry(3),
+      catchError(this.handleError)
+    );
+  }
 }
